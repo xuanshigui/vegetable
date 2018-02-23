762724de4e630bddc7007753fd40a079dab856c1
zhenbianshu <zhenbianshu@foxmail.com>
2018-02-23 09:22:08 +0800

    使用自动注入的jdbcTemplate;

--- admin/src/main/java/com/aquatic/AquaticStarter.java
+++ admin/src/main/java/com/aquatic/AquaticStarter.java
@@ -2,12 +2,8 @@ package com.aquatic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@ImportResource(locations = {"classpath:db.xml"})
+@SpringBootApplication
 public class AquaticStarter {
     public static void main(String[] args) {
         SpringApplication.run(AquaticStarter.class, args);
