53d19580d027e9ed9ef96f18d1ad57830e3134c8
zhenbianshu <zhenbianshu@foxmail.com>
2018-03-11 15:18:07 +0800

    文件上传实现；

--- web/src/main/java/com/aquatic/controller/ProcessController.java
+++ web/src/main/java/com/aquatic/controller/ProcessController.java
@@ -1,13 +1,18 @@
 package com.aquatic.controller;
 
+import com.aquatic.service.FileService;
+import com.aquatic.service.preprocessing.process.Preprocessing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
+import java.io.*;
 import java.util.Map;
 
 /**
@@ -16,30 +21,34 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/")
 public class ProcessController extends BaseController {
-    @RequestMapping(value = "/upload_atmosphere.json", method = RequestMethod.POST)
-    public Map<String, Object> uploadAtmosphere(HttpServletRequest request, HttpServletResponse response) {
-        List<String> fields = Arrays.asList("name", "lprice", "mprice", "hprice", "classify", "unit", "date");
-        Map<String, String> data = buildData(request, fields);
-        String type = request.getParameter("type");
+    private final FileService fileService;
 
-        return buildResponse(null);
+    @Autowired
+    public ProcessController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @RequestMapping(value = "/upload_atmosphere.json", method = RequestMethod.POST)
+    public Map<String, Object> uploadAtmosphere(@RequestParam("file") MultipartFile file) {
+        boolean res = fileService.uploadFile(file, "atmosphere.csv");
+        return buildResponse(res);
     }
 
     @RequestMapping(value = "/upload_water.json", method = RequestMethod.POST)
-    public Map<String, Object> uploadWater(HttpServletRequest request, HttpServletResponse response) {
-        List<String> fields = Arrays.asList("name", "lprice", "mprice", "hprice", "classify", "unit", "date");
-        Map<String, String> data = buildData(request, fields);
-        String type = request.getParameter("type");
-
-        return buildResponse(null);
+    public Map<String, Object> uploadWater(@RequestParam("file") MultipartFile file) {
+        boolean res = fileService.uploadFile(file, "fiveall.csv");
+        return buildResponse(res);
     }
 
     @RequestMapping(value = "/process.json", method = RequestMethod.GET)
     public Map<String, Object> add(HttpServletRequest request, HttpServletResponse response) {
-        List<String> fields = Arrays.asList("name", "lprice", "mprice", "hprice", "classify", "unit", "date");
-        Map<String, String> data = buildData(request, fields);
-        String type = request.getParameter("type");
+        boolean res = false;
+        try {
+            Preprocessing.preprocessing();
+            res = true;
+        } catch (Exception e) {
+        }
 
-        return buildResponse(null);
+        return buildResponse(res);
     }
 }
