2cccbcfcbfaf8e2f6805dda2fde1b03cc2abdb66
zhenbianshu <zhenbianshu@foxmail.com>
2018-05-18 15:55:53 +0800

    updated data

--- web/src/main/java/com/aquatic/AquaticStarter.java
+++ web/src/main/java/com/aquatic/AquaticStarter.java
@@ -1,8 +1,6 @@
 package com.aquatic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class AquaticStarter {
     public static void main(String[] args) {
