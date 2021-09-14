ba3d101cd88e7c4baeff9a885822a51a9710843f
lyr <lyiran06@163.com>
2021-09-14 15:46:22 +0800

    move to vegetable;

--- web/src/main/java/com/vege/VegetableStarter.java
+++ web/src/main/java/com/vege/VegetableStarter.java
@@ -2,8 +2,8 @@ package com.vege;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class AquaticStarter {
+public class VegetableStarter {
     public static void main(String[] args) {
-        SpringApplication.run(AquaticStarter.class, args);
+        SpringApplication.run(VegetableStarter.class, args);
     }
 }
